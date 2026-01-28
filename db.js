daf600a0dfb6fc350781dc42d83c2ff8fd3c03aa
marcialjean429 <marcialjean429@gmail.com>
2026-01-27 23:29:31 -0500

    Update db.js
    modification dans la base de donne

--- db.js
+++ db.js
@@ -1,16 +1,21 @@
-require('dotenv').config();
-const {Pool} = require('pg');
+ require('dotenv').config();
+ const {Pool} = require('pg');
 
- const pool=new Pool({
-     host: process.env.DB_HOST,
-     port: process.env.DB_PORT || 5432,
-     user: process.env.DB_USER,
-     password: process.env.DB_PASSWORD,
-     database:process.env.DB_NAME,
- });
+//  const pool=new Pool({
+//      host: process.env.DB_HOST,
+//      port: process.env.DB_PORT || 5432,
+//      user: process.env.DB_USER,
+//      password: process.env.DB_PASSWORD,
+//      database:process.env.DB_NAME,
+//  });
 
-pool.connect()
-    .then(() =>  console.log('Connection'))
-    .catch(err=> console.error('erreur lors de la connection du db',err));
+const pool =new Pool({
+ connectionString: process.env.DATABASE_URL,
+ ssl: { rejectUnauthorized: false }
+});
+
+ pool.connect()
+     .then(() =>  console.log('Connection'))
+     .catch(err=> console.error('erreur lors de la connection du db',err));
         
-module.exports={pool};
+ module.exports={pool};
